b60d6540aa153b4a24e54febba68c81bffaedc2a
Todd <ttarcher@outlook.com>
2018-12-21 01:27:04 +0800

    :sparkles: add convert

---
+++ app/src/test/java/io/github/ttarcher/martian/ConvertUnitTest.kt
@@ -0,0 +1,11 @@
+package io.github.ttarcher.martian
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ConvertUnitTest {
+    @Test
+    fun mconvert_isCorrect() {
+        assertEquals("恻鉽", "测试".mconvert())
+    }
+}
